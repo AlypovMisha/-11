bc3e390ac259c46dbd1d0a2cc8d91c267121aac3
AlypovMisha <144989572+AlypovMisha@users.noreply.github.com>
2024-04-08 00:12:27 +0500

    Update TestCollections.cs

--- TestCollections.cs
+++ TestCollections.cs
@@ -1,10 +1,7 @@
-﻿using LibraryForLabs;
+using LibraryForLabs;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Лабораторная_работа_11
 {
@@ -22,7 +19,7 @@ namespace Лабораторная_работа_11
             linkedListString = new LinkedList<string>();
             dictionaryCar = new Dictionary<Cars, PassengerCar>();
             dictionaryStringCar = new Dictionary<string, PassengerCar>();
-            
+
             for (int i = 0; i < numberOfElements; i++)
             {
                 PassengerCar passengerCar = new PassengerCar();
@@ -37,17 +34,17 @@ namespace Лабораторная_работа_11
                 }
                 catch
                 {
-                    Console.WriteLine("Все гг");
+                    Console.WriteLine("Объекты с уникальными значениями не могут быть созданы. Все возможные комбинации были добавлены.");
                 }
             }
 
         }
-        
+
         public void PrintElements()
         {
-            Console.WriteLine("Первый элемент: "+linkedListCars.First.Value);
+            Console.WriteLine("Первый элемент: " + linkedListCars.First.Value);
             Console.WriteLine("Средний элемент: " + GetMiddleElement(linkedListCars));
-            Console.WriteLine("Последний элемент: " + linkedListCars.Last.Value);            
+            Console.WriteLine("Последний элемент: " + linkedListCars.Last.Value);
         }
 
         public PassengerCar ReturnFirstCar()
@@ -57,7 +54,7 @@ namespace Лабораторная_работа_11
 
         public Cars ReturnFirstCarForDictionary()
         {
-            return linkedListCars.First.Value.BaseCar;
+            return ReturnFirstCar().BaseCar;
         }
 
         public PassengerCar ReturnLastCar()
@@ -67,25 +64,25 @@ namespace Лабораторная_работа_11
 
         public Cars ReturnLastCarForDictionary()
         {
-            return linkedListCars.Last.Value.BaseCar;
+            return ReturnLastCar().BaseCar;
         }
 
-        public PassengerCar ReturnMiddleCar() 
+        public PassengerCar ReturnMiddleCar()
         {
             return GetMiddleElement(linkedListCars);
         }
 
         public Cars ReturnMiddleCarForDictionary()
         {
-            return GetMiddleElement(linkedListCars).BaseCar;
+            return ReturnMiddleCar().BaseCar;
         }
 
         // Метод для измерения времени поиска элемента в коллекции LinkedList<PassengerCar>
         public void SearchInListCars(PassengerCar car)
         {
-            sw.Reset();
-            sw.Start();
-            bool isContain = linkedListCars.Contains(car);
+            PassengerCar k = new PassengerCar(car.Brand, car.ReleaseYear, car.Color, car.Cost, car.Clearance, car.NumberOfSeats, car.TopSpeed, car.id.number);
+            sw.Restart();
+            bool isContain = linkedListCars.Contains(k);
             sw.Stop();
             if (isContain)
             {
@@ -100,9 +97,9 @@ namespace Лабораторная_работа_11
         // Метод для измерения времени поиска элемента в коллекции LinkedList<string>
         public void SearchInListCarsString(string carStr)
         {
-            sw.Reset();
-            sw.Start();
-            bool isContain = linkedListString.Contains(carStr);
+            string k = carStr;
+            sw.Restart();
+            bool isContain = linkedListString.Contains(k);
             sw.Stop();
             if (isContain)
             {
@@ -115,12 +112,11 @@ namespace Лабораторная_работа_11
         }
 
         // Метод для измерения времени поиска элемента в коллекции Dictionary
-        public void SearchDictionaryCarsKey(Cars key)
+        public void SearchDictionaryCarsKey(Cars car)
         {
-            
-            sw.Reset();
-            sw.Start();
-            bool isContain = dictionaryCar.ContainsKey(key);
+            Cars k = new Cars(car.Brand,car.ReleaseYear, car.Color, car.Cost, car.Clearance);
+            sw.Restart();
+            bool isContain = dictionaryCar.ContainsKey(k);
             sw.Stop();
             if (isContain)
             {
@@ -132,11 +128,11 @@ namespace Лабораторная_работа_11
             }
         }
         // Метод для измерения времени поиска элемента в коллекции Dictionary
-        public void SearchDictionaryCarsValue(PassengerCar key)
+        public void SearchDictionaryCarsValue(PassengerCar car)
         {
-            sw.Reset();
-            sw.Start();
-            bool isContain = dictionaryCar.ContainsValue(key);
+            PassengerCar k = new PassengerCar(car.Brand, car.ReleaseYear, car.Color, car.Cost, car.Clearance, car.NumberOfSeats, car.TopSpeed, car.id.number);
+            sw.Restart();
+            bool isContain = dictionaryCar.ContainsValue(k);
             sw.Stop();
             if (isContain)
             {
@@ -149,11 +145,11 @@ namespace Лабораторная_работа_11
         }
 
         // Метод для измерения времени поиска элемента в коллекции Dictionary
-        public void SearchDictionaryCarsKeyString(string key)
+        public void SearchDictionaryCarsKeyString(string carStr)
         {
-            sw.Reset();
-            sw.Start();
-            bool isContain = dictionaryStringCar.ContainsKey(key);
+            string k = carStr;
+            sw.Restart();
+            bool isContain = dictionaryStringCar.ContainsKey(k);
             sw.Stop();
             if (isContain)
             {
@@ -165,11 +161,11 @@ namespace Лабораторная_работа_11
             }
         }
         // Метод для измерения времени поиска элемента в коллекции Dictionary
-        public void SearchDictionaryCarsValueString(PassengerCar key)
+        public void SearchDictionaryCarsValueString(PassengerCar car)
         {
-            sw.Reset();
-            sw.Start();
-            bool isContain = dictionaryStringCar.ContainsValue(key);
+            PassengerCar k = new PassengerCar(car.Brand, car.ReleaseYear, car.Color, car.Cost, car.Clearance, car.NumberOfSeats, car.TopSpeed, car.id.number);
+            sw.Restart();
+            bool isContain = dictionaryStringCar.ContainsValue(k);
             sw.Stop();
             if (isContain)
             {
@@ -180,24 +176,19 @@ namespace Лабораторная_работа_11
                 Console.WriteLine($"Элемент не найден за {sw.ElapsedTicks}");
             }
         }
-
-
         private static T GetMiddleElement<T>(LinkedList<T> linkedList)
         {
             if (linkedList == null || linkedList.Count == 0)
             {
                 throw new ArgumentException("Список пуст.");
             }
-
             LinkedListNode<T> slow = linkedList.First;
             LinkedListNode<T> fast = linkedList.First;
-
             while (fast != null && fast.Next != null)
             {
                 slow = slow.Next;
                 fast = fast.Next.Next;
             }
-
             return slow.Value;
         }
 
